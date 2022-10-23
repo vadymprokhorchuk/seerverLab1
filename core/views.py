f453b7dd79073ee896dfe5bb0cb372d0d470fbc0
Vadym <ia03.prokhorchuk.vadym@gmail.com>
2022-10-23 23:06:32 +0300

    Add initial data

--- core/views.py
+++ core/views.py
@@ -1,5 +1,31 @@
 from core import app
 
+
+USERS = [
+    {
+        "id": 1,
+        "name": "John"
+    }
+]
+
+CATEGORIES = [
+    {
+        "id": 1,
+        "name": "Food"
+    }
+]
+
+RECORDS = [
+    {
+        "id": 1,
+        "user_id": 1,
+        "category_id": 1,
+        "created": 1666544270,
+        "amount": 280
+    }
+]
+
+
 @app.route("/")
 def index():
     return "Flask app"
